a1f7e2419f16afed4c2a79d1e59da6bd3a94edbe
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-12 19:25:42 -0400

    color fixes

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -5,7 +5,7 @@ export default function Title() {
             <p className="font-bold text-md text-white">
 				hello! i&apos;m</p>
 			<h1 className="text-4xl sm:text-4xl font-bold 
-				bg-gradient-to-r from-blue-300 to-blue-700 
+				bg-gradient-to-r from-blue-300 to-blue-600 
 				bg-clip-text text-transparent">
 				mustaeen ahmed
 			</h1>
