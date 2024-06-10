889251d074dbbe4382a778bd36ffcde69d8acee6
must108 <136145181+must108@users.noreply.github.com>
2024-06-10 19:09:44 -0400

    fun

---
+++ src/app/sad/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <div className="flex items-center
+        justify-center h[100vh]">
+            <Image src="sad.jpg"
+            alt="sad"
+            width="360"
+            height="356" 
+            />
+        </div>
+    )
+}
