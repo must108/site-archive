4cc69809551cd6b090dc60b251b2802ad2177146
must108 <136145181+must108@users.noreply.github.com>
2024-06-10 19:11:29 -0400

    small centering error

--- src/app/sad/page.tsx
+++ src/app/sad/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Page() {
     return (
         <div className="flex items-center
-        justify-center h[100vh]">
+        justify-center h-[100vh]">
             <Image src="sad.jpg"
             alt="sad"
             width="360"
