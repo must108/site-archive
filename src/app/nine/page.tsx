0baedc3301e0b36b5afad8e3aa3f6df3507cd1ad
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-12 17:51:31 -0400

    fixed random images

--- src/app/nine/page.tsx
+++ src/app/nine/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div className="flex items-center
         justify-center h-[100vh]">
-            <Image src={process.env.IMAGE_URL!} 
+            <Image src="/nine.png"
             alt="forgive me" 
             width="486" 
             height="573" 
--- src/app/sad/page.tsx
+++ src/app/sad/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div className="flex items-center
         justify-center h-[100vh]">
-            <Image src="sad.jpg"
+            <Image src="/sad.jpg"
             alt="sad"
             width="360"
             height="356" 
